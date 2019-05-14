e4122689269af486eaa9ebeb6c37ceabf41f8e9d
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-14 13:09:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,14 +1,13 @@
-
- 
 def badge_maker(name)
 "Hello, my name is #{name}."
 end 
 
 def batch_badge_creator(attendees)
   attendees.each do |name| 
- puts "Hello, my name is #{name}." 
+ "Hello, my name is #{name}." 
 end
 end 
+
 
 
   
