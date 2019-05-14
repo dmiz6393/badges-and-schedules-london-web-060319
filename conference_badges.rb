f97ed0305203fc1f9066c90b67926019ea5f0de2
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-14 13:31:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 def assign_rooms(attendees)
   new_list=[]
 attendees.each_with_index do |attendee, index|
-"Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+new_list<< "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
 end
 end
   
