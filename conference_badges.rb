253dd0a6705b53243122d309ab4b26a8454f1c11
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-14 13:04:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,15 @@
+attendees=["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+ 
+def badge_maker(name)
+"Hello, my name is #{name}."
+end 
 
+def batch_badge_creator(attendees)
+  attendees.each {|x| puts "Hello, my name is #{name}."}
+end
+
+def assign_rooms(speakers)
+  #"Hello, _____! You'll be assigned to room _____!"# 
   
   
   
