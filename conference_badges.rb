0378d3e8144d17a2c5007f6c44058c4fb16c24d7
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-14 13:22:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,13 +8,13 @@ end
 
 def batch_badge_creator(attendees)
  attendees.map do |attendee|
-"Hello, my name is #{attendee}."
+puts "Hello, my name is #{attendee}."
 end
 end
 
 def assign_rooms(attendees)
 attendees.each_with_index do |attendee, index|
-"Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+puts "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
 end
 end
   
